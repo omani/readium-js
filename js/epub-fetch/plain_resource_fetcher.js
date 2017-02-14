868639e0a02b4ccdd0c800b7250035efbd5279f9
Andy Hubert <andy.hubert@gmail.com>
2017-02-14 10:32:35 +0200

    Setup proper skip of encryption.xml and apple display file, with localstorage trigger

--- js/epub-fetch/plain_resource_fetcher.js
+++ js/epub-fetch/plain_resource_fetcher.js
@@ -11,7 +11,7 @@
 //  used to endorse or promote products derived from this software without specific 
 //  prior written permission.
 
-define(['jquery', 'URIjs', './discover_content_type'], function ($, URI, ContentTypeDiscovery) {
+define(['jquery', 'URIjs', './discover_content_type', 'biblemesh_Settings'], function ($, URI, ContentTypeDiscovery, Settings) {
 
     var PlainResourceFetcher = function(parentFetcher){
 
@@ -76,28 +76,40 @@ define(['jquery', 'URIjs', './discover_content_type'], function ($, URI, Content
                 throw 'Fetched file URL is undefined!';
             }
 
-            if(fileUrl.match(/(\/META-INF\/encryption\.xml|\/META-INF\/com\.apple\.ibooks\.display-options\.xml)$/)) {
-                onerror();
-                return;
-            }
+            var optionalFetch = (pathRelativeToPackageRoot.indexOf("META-INF/com.apple.ibooks.display-options.xml") == 0)
+            || (pathRelativeToPackageRoot.indexOf("META-INF/encryption.xml") == 0);
 
-            $.ajax({
-                // encoding: "UTF-8",
-                // mimeType: "text/plain; charset=UTF-8",
-                // beforeSend: function( xhr ) {
-                //     xhr.overrideMimeType("text/plain; charset=UTF-8");
-                // },
-                isLocal: fileUrl.indexOf("http") === 0 ? false : true,
-                url: fileUrl,
-                dataType: 'text', //https://api.jquery.com/jQuery.ajax/
-                async: true,
-                success: function (result) {
-                    fetchCallback(result);
-                },
-                error: function (xhr, status, errorThrown) {
-                    onerror(new Error(errorThrown));
+            Settings.get('404:' + fileUrl, function(is404) {
+
+                if(optionalFetch && is404) {
+                    console.log("Skipped fetch of " + fileUrl + " due to presence of localstorage value: " + '404:' + fileUrl);
+                    onerror();
+                    return;
                 }
-            });
+
+                $.ajax({
+                    // encoding: "UTF-8",
+                    // mimeType: "text/plain; charset=UTF-8",
+                    // beforeSend: function( xhr ) {
+                    //     xhr.overrideMimeType("text/plain; charset=UTF-8");
+                    // },
+                    isLocal: fileUrl.indexOf("http") === 0 ? false : true,
+                    url: fileUrl,
+                    dataType: 'text', //https://api.jquery.com/jQuery.ajax/
+                    async: true,
+                    success: function (result) {
+                        fetchCallback(result);
+                    },
+                    error: function (xhr, status, errorThrown) {
+                        if(optionalFetch) {
+                            Settings.put('404:' + fileUrl, 1)
+                        }
+                        onerror(new Error(errorThrown));
+                    }
+                });
+
+            })
+            
         };
 
         this.fetchFileContentsBlob = function(pathRelativeToPackageRoot, fetchCallback, onerror) {
